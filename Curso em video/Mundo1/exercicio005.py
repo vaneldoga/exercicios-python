3f8220b2ce5769fc91c914df74e0e22252191b97
Yuri Delgado <vaneldoga@gmail.com>
2023-12-03 23:18:40 -0300

    add exercicio005.py

---
+++ Curso em video/Mundo1/exercicio005.py
@@ -0,0 +1,9 @@
+"""
+Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+"""
+
+n = int(input("Digite um número: "))
+
+print(f"Número digitado: {n}")
+
+print(f"Sucessor de {n}: {n+1}\nAntecessor de {n}: {n-1}")
