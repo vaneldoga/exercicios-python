3a9f76839635f5dde432ffa81e8eb01e1fca13a3
Yuri Delgado <vaneldoga@gmail.com>
2023-12-03 23:21:23 -0300

    update exercicio005.py

--- Curso em video/Mundo1/exercicio005.py
+++ Curso em video/Mundo1/exercicio005.py
@@ -4,6 +4,9 @@ Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e 
 
 n = int(input("Digite um número: "))
 
-print(f"Número digitado: {n}")
+print(f"""
+      Número digitado: {n}
 
-print(f"Sucessor de {n}: {n+1}\nAntecessor de {n}: {n-1}")
+      Sucessor de {n}: {n+1}
+      Antecessor de {n}: {n-1}
+      """)
